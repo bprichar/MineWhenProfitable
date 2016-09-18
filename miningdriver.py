54675b5a9eed21b4c2dfbf6c20923d8cda7bad4f
Brad Richardson <brad.richardson@transware.net>
2016-09-18 12:57:09 -0500

    Complete mining driver

--- miningdriver.py
+++ miningdriver.py
@@ -7,10 +7,15 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
 from time import sleep
+import thread
+import os
 
 TRANSMISSION_CHARGE = 0.04 # $/kWh
 POWER_CONSUMPTION = 95.0 # W
 HASH_RATE = 120.0 # H/s
+already_mining = False
+current_currency = ""
+mining_thread = None
 
 def getElectricityPrice():
     baseaddress = 'https://rrtp.comed.com/api?type=5minutefeed&datestart='
@@ -60,11 +65,48 @@ def getMaxReward():
             max_rate = rewards[currency]
     return max_currency, max_rate
 
-electricity_price = getElectricityPrice()
-cost_rate = electricity_price * (POWER_CONSUMPTION / HASH_RATE) / 3600 # $/kH
-currency, reward_rate = getMaxReward()
-profit = reward_rate - cost_rate # $/kH
+def mine_function(currency):
+    mine_command = "/usr/bin/minergate-cli -user bprvxf@gmail.com -" + currency + " 3"
+    print "Going to mine", currency
+    print mine_command
+    os.system(mine_command)
 
-print "Cost Rate = $", cost_rate, "/kH"
-print "Reward Rate = $", reward_rate, "/kH, mining", currency
-print "Profit = $", profit, "/kH"
+def mine(currency):
+    global current_currency
+    global mining_thread
+    global already_mining
+    if already_mining and currency == current_currency:
+        return
+    elif already_mining:
+        mining_thread.exit()
+        mining_thread = thread.start_new_thread(mine_function, currency)
+        current_currency = currency
+    else:
+        mining_thread = thread.start_new_thread(mine_function, (currency,))
+        already_mining = True
+        current_currency = currency
+    
+
+def stop_mining():
+    global mining_thread
+    global already_mining
+    if already_mining:
+        mining_thread.exit()
+        already_mining = False
+
+while True:
+    electricity_price = getElectricityPrice()
+    cost_rate = electricity_price * (POWER_CONSUMPTION / HASH_RATE) / 3600 # $/kH
+    currency, reward_rate = getMaxReward()
+    profit = reward_rate - cost_rate # $/kH
+
+    print "Cost Rate = $", cost_rate, "/kH"
+    print "Reward Rate = $", reward_rate, "/kH, mining", currency
+    print "Profit = $", profit, "/kH"
+
+    if profit > 0:
+        mine(currency)
+    else:
+        stop_mining()
+
+    sleep(300)
